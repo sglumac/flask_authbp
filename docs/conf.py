f0db752cfe1bb33a119ec012307c29c5cb1674e5
Slaven Glumac <slaven.glumac@gmail.com>
2022-06-06 14:05:02 +0200

    autodoc_mock_imports

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,8 @@ sys.path.insert(0, os.path.abspath('..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
+autodoc_mock_imports = ['flask', 'flask_restx']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
